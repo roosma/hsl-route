6f14c896ee2255c77d7b36f06b1f98691a8d6154
Martin Roos <martin.roos@nordsoftware.com>
2019-01-20 15:29:21 +0200

    Updates

---
+++ precache-manifest.9270ca87db75adc6d2345bb7bcd78d72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bcc12b313bd5bcb7a23",
+    "url": "/hsl-route/static/js/runtime~main.9bcc12b3.js"
+  },
+  {
+    "revision": "9aca42f179451bfaedd7",
+    "url": "/hsl-route/static/js/main.9aca42f1.chunk.js"
+  },
+  {
+    "revision": "307ed6f03333603d5baf",
+    "url": "/hsl-route/static/js/1.307ed6f0.chunk.js"
+  },
+  {
+    "revision": "9aca42f179451bfaedd7",
+    "url": "/hsl-route/static/css/main.77659f00.chunk.css"
+  },
+  {
+    "revision": "227dc165fafd5e0b1a4031784ea78a9b",
+    "url": "/hsl-route/index.html"
+  }
+];
